e231a5fa3a3335f2f7782944d72e90ffdd32ad6a
Todolist <61679602+youngsjung@users.noreply.github.com>
2020-04-13 17:58:03 +0900

    Revert "에러 수정"
    This reverts commit 1499db55a72eb531bd7fc37d2b78011cb5d968d9.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,31 +1,35 @@
 <template>
-  <section>
-    <ul>
-      <!-- 
-            보간법을 사용할때 괄호를 {{ } }가 아닌 {{ }} 처럼 붙여 써야합니다.
-            v-for을 사용 할 때는 key 값을 추가해 줘야 합니다.
-            :key는 v-bind:key와 같은 의미 입니다. (약어로 사용한 것 입니다.)
-        -->
-      <li v-for="todoItem in todoItems" :key="todoItem">{{ todoItem }}</li>
-    </ul>
-  </section>
+    
+<section> 
+<ul>
+ <li v-for="todoItem in todoItems"> {{ todoItem } } </li>
+</ul>
+</section>
+
+
+
 </template>
+
 
 <script>
 export default {
-  data() {
-    return {
-      todoItems: []
-    };
-  },
-  created() {
-    if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
-        this.todoItem.push(localStorage.key(i));
-      }
+    data(){
+        return{
+            todoItems: []
+        }
+    },
+    created(){
+        if(localStorage.length > 0){
+            for(var i =0; i < localStorage.length; i++){
+                this.todoItem.push(localStorage.key(i));
+            }
+        }
     }
-  }
-};
+    
+}
+
 </script>
 
-<style></style>
+<style >
+
+</style>
